a11363b4f325e749d37c0d029cd0e93a89757d48
Cheng Shi <shicheng107@hotmail.com>
2015-01-08 22:54:29 +0000

    The easiest solution.

---
+++ 133CloneGraph/solution1.cpp
@@ -0,0 +1,15 @@
+/**
+ * Cheng Shi, shicheng107@hotmail.com
+ */
+
+#include <string>
+
+std::string CloneGraph(const std::string &g)
+{
+    return g;
+}
+
+int main(int argc, const char** argv)
+{
+	return 0;
+}
